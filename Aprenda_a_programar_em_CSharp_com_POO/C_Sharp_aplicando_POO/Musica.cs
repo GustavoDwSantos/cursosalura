c7efc478433814471aa65c0b62960aede4629d83
GustavoDwSantos <gustavos200116@gmail.com>
2024-01-17 18:18:14 -0300

    Adição CSharp: Aplicando a Orientação a Objetos -Adição Referente a Aula 1

---
+++ Aprenda_a_programar_em_CSharp_com_POO/C_Sharp_aplicando_POO/Musica.cs
@@ -0,0 +1,19 @@
+class Musica
+{
+    public string nome;
+    public string artista;
+    public int duracao;
+    public bool disponivel;
+
+    public void ExibirFichaTecnica(){
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração: {duracao}");
+        if(disponivel){
+            Console.WriteLine("Disponivel no plano.");
+        }
+        else{
+            Console.WriteLine("Não Disponivel. Adiquira o plano+");
+        }
+    }
+}
---
+++ Aprenda_a_programar_em_CSharp_com_POO/C_Sharp_aplicando_POO/Program.cs
@@ -0,0 +1,17 @@
+﻿// Projeto de musicas
+
+Musica musica1 = new Musica();
+musica1.nome = "Brain Damage";
+musica1.artista = "Pink Floyd";
+musica1.duracao = 173;
+musica1.disponivel = true;
+
+Musica musica2 = new Musica();
+musica2.nome = "Oia O Rappa";
+musica2.artista = "O Rappa";
+musica2.duracao = 312;
+musica2.disponivel = false;
+
+musica1.ExibirFichaTecnica();
+
+musica2.ExibirFichaTecnica();
