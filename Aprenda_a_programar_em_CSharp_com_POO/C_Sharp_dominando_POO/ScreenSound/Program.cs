d3c6e24502cfe69dc134230071a7a282d38926cd
GustavoDwSantos <gustavos200116@gmail.com>
2024-01-19 23:26:26 -0300

    Adição CSharp: Dominando a orientação a objetos -Adição referente a aula 03

---
+++ Aprenda_a_programar_em_CSharp_com_POO/C_Sharp_dominando_POO/ScreenSound/Program.cs
@@ -0,0 +1,57 @@
+﻿using ScreenSound.Modelos;
+using ScreenSound.Menus;
+
+Banda beatles = new("The Beatles");
+Banda rappa = new("O Rappa");
+
+Dictionary<string, Banda> bandasRegistradas = new();
+bandasRegistradas.Add(rappa.Nome, rappa);
+bandasRegistradas.Add(beatles.Nome, beatles);
+
+Dictionary<int, Menu> menus = new();
+menus.Add(1,new MenuRegistrarBanda());
+menus.Add(2, new MenuRegistrarAlbum());
+menus.Add(3, new MenuMostrarBandasRegistradas());
+menus.Add(4, new MenuAvaliarUmaBanda());
+menus.Add(5, new MenuExibirDetalhes());
+menus.Add(-1, new MenuSair());
+
+void ExibirLogo()
+{
+    Console.WriteLine(@"
+
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+");
+    Console.WriteLine("Boas vindas ao Screen Sound 2.0!");
+}
+
+void ExibirOpcoesDoMenu()
+{
+    ExibirLogo();
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para registrar o álbum de uma banda");
+    Console.WriteLine("Digite 3 para mostrar todas as bandas");
+    Console.WriteLine("Digite 4 para avaliar uma banda");
+    Console.WriteLine("Digite 5 para exibir os detalhes de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("\nDigite a sua opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+    if (menus.ContainsKey(opcaoEscolhidaNumerica)){
+        menus[opcaoEscolhidaNumerica].Executar(bandasRegistradas);
+        if (opcaoEscolhidaNumerica != -1){
+            ExibirOpcoesDoMenu();
+        }
+    }
+    else {
+        Console.WriteLine("Opção inválida");
+    }
+}
+ExibirOpcoesDoMenu();
