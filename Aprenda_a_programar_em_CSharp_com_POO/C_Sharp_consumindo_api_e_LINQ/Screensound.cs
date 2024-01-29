af7ce4fc4820bd73da1da59511107ec9bb580a95
GustavoDwSantos <gustavos200116@gmail.com>
2024-01-29 11:05:34 -0300

    Adição CSharp: consumindo API e gravando arquivos -Adição referente a aula 02

---
+++ Aprenda_a_programar_em_CSharp_com_POO/C_Sharp_consumindo_api_e_LINQ/Screensound.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+using ScreenSound_04.Modelos;
+
+using (HttpClient client = new HttpClient())
+{
+    try{
+    string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json");
+    // Console.WriteLine(resposta);
+    var musicas = JsonSerializer.Deserialize<List<Musica>>(resposta)!;
+    Console.WriteLine(musicas.Count);
+    musicas[20].ExibirDetalhesDaMusica();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Temos um problema: {ex.Message}");
+    }
+}
+
